6db156097f9f15831e8162ff568b208f3c50c4a3
fukpyd <aniafukas@interia.pl>
2023-09-18 14:07:16 +0200

    create js modules

---
+++ js/utils.js
@@ -0,0 +1,8 @@
+export function toggleTheme(rootElement, themes) {
+  rootElement.classList.toggle("dark");
+  let currentTheme = themes.DARK;
+  if (!rootElement.classList.contains("dark")) {
+    currentTheme = themes.LIGHT;
+  }
+  localStorage.setItem("theme", currentTheme);
+}
