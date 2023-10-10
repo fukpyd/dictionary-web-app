fbbf973279266baed95e70be7b91db8a414b2470
fukpyd <aniafukas@interia.pl>
2023-10-10 17:09:48 +0200

    modify index.js file

--- js/index.js
+++ js/index.js
@@ -1,12 +1,14 @@
 import { THEMES } from "./constants.js";
 import { toggleTheme } from "./utils.js";
-// import { getData } from "./api.js";
+import { getData } from "./api.js";
 import {
   createResult,
   createResultHeader,
   showNoResult,
   createFooter,
   changeFontStyle,
+  showNoQuery,
+  clearResultWrapper,
 } from "./functions.js";
 
 const themeButton = document.querySelector(".theme-input");
@@ -15,9 +17,11 @@ const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
 const root = document.documentElement;
 const themeInput = document.querySelector(".theme-input");
 const url = `https://api.dictionaryapi.dev/api/v2/entries/en`;
-let play = 0;
 const resultWrapper = document.querySelector(".result-wrapper");
 const select = document.querySelector(".font-select");
+const searchButton = document.querySelector(".search-button");
+const searchInput = document.querySelector(".search-input");
+const inputClass = "no-query-message";
 
 if (currentTheme === "DARK" || prefersDarkScheme.matches) {
   document.documentElement.classList.toggle("dark");
@@ -28,27 +32,44 @@ themeButton.addEventListener("click", () => toggleTheme(root, THEMES));
 
 select.addEventListener("change", changeFontStyle);
 
-async function getData(url, query) {
-  try {
-    const response = await fetch(`${url}/${query}`);
-    const data = await response.json();
+const handleSubmit = async function (event, url, query, element, elementClass) {
+  event.preventDefault();
+  resultWrapper.innerHTML = "";
 
-    if (!Array.isArray(data) && !data?.length) {
-      showNoResult(data, query);
-      return;
-    }
-    const resultHeader = createResultHeader(data);
-    const result = createResult(data);
-    const footer = createFooter(data);
-    resultWrapper.insertAdjacentElement("afterbegin", resultHeader);
-    resultWrapper.insertAdjacentHTML("beforeend", result);
-    resultWrapper.insertAdjacentHTML("beforeend", footer);
-  } catch (error) {
-    console.log("Problem with server. Please contact administrator");
+  if (element.classList.contains(elementClass)) {
+    element.classList.remove(elementClass);
   }
-}
 
-// getData(url, "music");
-getData(url, "hello");
-// getData(url, "accomplishment");
-// getData(url, "keyboard");
+  if (!query) {
+    const noQueryMessage = showNoQuery(searchInput, inputClass, resultWrapper);
+    resultWrapper.insertAdjacentHTML("afterbegin", noQueryMessage);
+    return;
+  }
+
+  const data = await getData(url, query);
+
+  if (!Array.isArray(data) && !data?.length) {
+    const noResultMessage = showNoResult(query, searchInput);
+    resultWrapper.insertAdjacentHTML("afterbegin", noResultMessage);
+    return;
+  }
+
+  const resultHeader = createResultHeader(data);
+  console.log(resultHeader);
+  const result = createResult(data);
+  const footer = createFooter(data);
+  resultWrapper.insertAdjacentElement("beforeend", resultHeader);
+  resultWrapper.insertAdjacentHTML("beforeend", result);
+  resultWrapper.insertAdjacentHTML("beforeend", footer);
+};
+
+searchButton.addEventListener("click", (event) =>
+  handleSubmit(event, url, searchInput.value, searchInput, inputClass)
+);
+
+searchButton.addEventListener("keyup", function (event) {
+  event.preventDefault();
+  if (event.keyCode === 13) {
+    handleSubmit(event, url, searchInput.value, searchInput, inputClass);
+  }
+});
