b286602a2e8949b76dab06bc1c79db5cb9b46bdd
fukpyd <aniafukas@interia.pl>
2023-10-10 17:09:04 +0200

    modify api.js file

--- js/api.js
+++ js/api.js
@@ -1,1 +1,9 @@
-
+export async function getData(url, query) {
+  try {
+    const response = await fetch(`${url}/${query}`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log("Problem with server. Please contact administrator");
+  }
+}
