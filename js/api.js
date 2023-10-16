f3a073c5099c7a9421e3543ecd39653c05934171
fukpyd <aniafukas@interia.pl>
2023-10-16 20:40:10 +0200

    add API_URL const to api.js

--- js/api.js
+++ js/api.js
@@ -1,9 +1,11 @@
+export const API_URL = 'https://api.dictionaryapi.dev/api/v2/entries/en';
+
 export async function getData(url, query) {
   try {
     const response = await fetch(`${url}/${query}`);
     const data = await response.json();
     return data;
   } catch (error) {
-    console.log("Problem with server. Please contact administrator");
+    console.log('Problem with server. Please contact administrator');
   }
 }
